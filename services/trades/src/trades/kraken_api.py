e55b0a67d113419a26ce25eb7bf94e9866af7016
Karim Abousselham <karim.abousselhaml@gmail.com>
2025-06-18 20:43:07 +0100

    Pushes trades form Kraken websocket to kafka

---
+++ services/trades/src/trades/kraken_api.py
@@ -0,0 +1,93 @@
+from typing import List
+from pydantic import BaseModel
+from websocket import create_connection
+import json
+from loguru import logger
+
+class Trade(BaseModel):
+    product_id: str
+    price: float
+    quantity: float
+    timestamp: str
+    
+    def to_dict(self):
+        """
+        Converts the Trade object to a dictionary.
+        
+        Returns:
+            dict: A dictionary representation of the Trade object.
+        """
+        return self.model_dump()
+    
+class KrakenAPI():
+    URL= "wss://ws.kraken.com/v2"
+    def __init__(
+        self,
+        product_ids: List[str] ,
+    ):
+        self.product_ids = product_ids 
+        # create a websocket client
+        self._ws_client=create_connection(self.URL)
+        
+        #subscribe to the websocket
+        self._subscribe(self.product_ids)
+    
+    def _subscribe(self, product_ids: List[str]):
+        """
+        Subscribes to the websocket and waits for the initial snapshot for the given `product_ids`.
+        """
+        # send a subscribe message to the websocket
+        self._ws_client.send(
+            json.dumps(
+                {
+                    'method': 'subscribe',
+                    'params': {
+                        'channel': 'trade',
+                        'symbol': self.product_ids,
+                        'snapshot': False,
+                    },
+                }
+            )
+        )
+    #In case of Kraken websocket you get two confirmation messages for each subscription
+    # so you have to discard them as they contain no relevant data
+        for _ in self.product_ids:
+            _ = self._ws_client.recv()
+            _ = self._ws_client.recv()
+        
+    def get_trades(self) -> List[Trade]:
+        """
+        Fetches trades from the Kraken websocket API.
+
+        Returns:
+            List[Trade]: A list of Trade objects containing trade data.
+        """
+        data: str = self._ws_client.recv()
+        if 'heartbeat' in data:
+            logger.info("Received heartbeat, skipping...")
+            return []
+        
+        # transform raw string into a JSON object
+        try:
+            data = json.loads(data)
+        except json.JSONDecodeError as e:
+            logger.error(f'Error decoding JSON: {e}')
+            return []
+        
+        try:
+            trades_data = data['data']
+        except KeyError as e:
+            logger.error(f'No `data` field with trades in the message {e}')
+            return []
+        
+        trades = [
+            Trade(
+                    product_id=trade['symbol'],
+                    price=float(trade['price']),
+                    quantity=float(trade['qty']),
+                    timestamp=trade['timestamp']
+                )
+            for trade in trades_data
+        ]
+        
+        return trades
--- services/trades/src/trades/main.py
+++ services/trades/src/trades/main.py
@@ -1,30 +1,50 @@
 # Create an Application instance with Kafka configs
 from quixstreams import Application
+from kraken_api import KrakenAPI, Trade
+from typing import List
+from loguru import logger
+
+def run(
+    kafka_broker_address: str,
+    kafka_topic_name: str,
+    kraken_api:KrakenAPI
+):
+    app = Application(
+        broker_address=kafka_broker_address, 
+    )
+
+    # Define a topic "my_topic" with JSON serialization
+    topic = app.topic(name=kafka_topic_name, value_serializer='json')
 
 
-app = Application(
-    broker_address='localhost:31234', 
-    consumer_group='example'
-)
 
-# Define a topic "my_topic" with JSON serialization
-topic = app.topic(name='my_topic', value_serializer='json')
+    # Create a Producer instance
+    with app.get_producer() as producer:
+        while True:
+            
+            events: List[Trade]=kraken_api.get_trades()
+            #event = {"id": "1", "text": "Lorem ipsum dolor sit amet"}
+            for event in events:
+                # Serialize an event using the defined Topic 
+                message = topic.serialize(
+                    # key=event["id"],
+                    value=event.to_dict()
+                    )
 
+                # Produce a message into the Kafka topic
+                producer.produce(
+                    topic=topic.name, 
+                    value=message.value, 
+                    #key=message.key
+                )
+                # Log the produced messages
+                logger.info(f"Produced message to topic: {topic.name}")
 
-
-# Create a Producer instance
-with app.get_producer() as producer:
-    while True:
-        event = {"id": "1", "text": "Lorem ipsum dolor sit amet"}
-        # Serialize an event using the defined Topic 
-        message = topic.serialize(key=event["id"], value=event)
-
-        # Produce a message into the Kafka topic
-        producer.produce(
-            topic=topic.name, 
-            value=message.value, 
-            key=message.key
-        )
-        #For debugging purposes
-        import time
-        time.sleep(1)  # Sleep for a second before sending the next message
+if __name__ == '__main__':
+    # create an instance of KrakenAPI to talk to Kraken websocket API
+    api= KrakenAPI(product_ids=["BTC/USD"])
+    run(
+        kafka_broker_address="localhost:9092",
+        kafka_topic_name="trades",
+        kraken_api=api
+    )
