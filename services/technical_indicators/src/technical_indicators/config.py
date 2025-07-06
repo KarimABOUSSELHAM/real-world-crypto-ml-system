48f94b70fb3e67176a061ee8825de76b0e7ce81b
Karim Abousselham <karim.abousselhaml@gmail.com>
2025-07-06 11:54:58 +0100

    Add boilerplate of the technical_indicators service

---
+++ services/technical_indicators/src/technical_indicators/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='services/technical_indicators/src/technical_indicators/settings.env',
+        evn_file_encoding='utf-8',
+    )
+
+    kafka_broker_address: str
+    kafka_input_topic: str
+    kafka_output_topic: str
+    kafka_consumer_group: str
+    candle_seconds: int
+
+
+config = Settings()
+# print(settings.model_dump())
---
+++ services/technical_indicators/src/technical_indicators/main.py
@@ -0,0 +1,72 @@
+from loguru import logger
+from quixstreams import Application
+
+
+def run(
+    kafka_broker_address: str,
+    kafka_input_topic: str,
+    kafka_output_topic: str,
+    kafka_consumer_group: str,
+    candle_seconds: int,
+):
+    """
+    Transforms a stream of input candles into a stream of output technical indicators.
+
+    In three steps:
+    - Ingests candles from `kafka_input_topic`
+    - Computes technical indicators
+    - Produces technical indicators to `kafka_output_topic`
+    Args:
+        kafka_broker_address (str): Address of the Kafka broker.
+        kafka_input_topic (str): Name of the Kafka topic to read candles from.
+        kafka_output_topic (str): Name of the Kafka topic to write technical indicators to.
+        kafka_consumer_group (str): Kafka consumer group name.
+        candle_seconds (int): Duration of each candle in seconds.
+
+    Returns:
+        None
+    """
+    app = Application(
+        broker_address=kafka_broker_address,
+        consumer_group=kafka_consumer_group,
+    )
+    # Define the input topic
+    candles_topic = app.topic(
+        name=kafka_input_topic,
+        value_serializer='json',
+    )
+    # Define the output topic
+    technical_indicators_topic = app.topic(
+        name=kafka_output_topic,
+        value_serializer='json',
+    )
+    # Step 1. Ingest candles from the input topic for the given `candle_seconds`
+    # Create a streaming dataframe connected to the input topic
+    sdf = app.dataframe(topic=candles_topic)
+
+    # Filter the candles for the given `candle_seconds`
+    sdf = sdf[sdf['candle_seconds'] == candle_seconds]
+
+    # Step 2. Compute technical indicators
+    # TODO: data processing
+
+    # Print the data
+    sdf = sdf.update(lambda message: logger.debug(f'Final message: {message}'))
+
+    # Step 3. Produce candles to the output topic
+    sdf = sdf.to_topic(topic=technical_indicators_topic)
+
+    # Start the streaming application
+    app.run()
+
+
+if __name__ == '__main__':
+    from technical_indicators.config import config
+
+    run(
+        kafka_broker_address=config.kafka_broker_address,
+        kafka_input_topic=config.kafka_input_topic,
+        kafka_output_topic=config.kafka_output_topic,
+        kafka_consumer_group=config.kafka_consumer_group,
+        candle_seconds=config.candle_seconds,
+    )
