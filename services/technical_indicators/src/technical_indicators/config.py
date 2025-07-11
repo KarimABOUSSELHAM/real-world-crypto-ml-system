b3d8e7355c386cc41eaf6510ddb4316aeaa328aa
Karim Abousselham <karim.abousselham@gmail.com>
2025-07-11 18:56:39 +0100

    Add dockerfile for the technical indicators service while adjusting paths for the config.py

--- services/technical_indicators/src/technical_indicators/config.py
+++ services/technical_indicators/src/technical_indicators/config.py
@@ -1,10 +1,18 @@
+import os
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = os.path.dirname(
+    __file__
+)  # /app/services/technical_indicators/src/technical_indicators
+ENV_FILE = os.path.join(BASE_DIR, 'settings.env')
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file='services/technical_indicators/src/technical_indicators/settings.env',
-        evn_file_encoding='utf-8',
+        env_file=ENV_FILE,
+        env_file_encoding='utf-8',
+        # case_sensitive=False,
     )
 
     kafka_broker_address: str
@@ -12,7 +20,7 @@ class Settings(BaseSettings):
     kafka_output_topic: str
     kafka_consumer_group: str
     candle_seconds: int
-    max_candles_in_state: int = 1000
+    max_candles_in_state: int = 100
 
 
 config = Settings()
