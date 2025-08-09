ab7196674952850b360c1452d3661a298ebcb866
Karim Abousselham <karim.abousselham@gmail.com>
2025-08-09 16:23:30 +0100

    Adds sentiment extractor function and refactors the main script

--- services/news-sentiment/src/news_sentiment/main.py
+++ services/news-sentiment/src/news_sentiment/main.py
@@ -3,17 +3,7 @@ from typing import List
 from loguru import logger
 from quixstreams import Application
 
-# from news_sentiment.candle import get_sentiment_scores
-
-
-def get_sentiment_scores(news_item: dict) -> List[dict]:
-    # TODO: Call the actual LLM API to get the sentiment scores
-    # But before let's mock the sentiment scores
-    timestamp_ms = news_item['timestamp_ms']
-    return [
-        {'coin': 'BTC', 'score': 1, 'timestamp_ms': timestamp_ms},
-        {'coin': 'ETH', 'score': -1, 'timestamp_ms': timestamp_ms},
-    ]
+from news_sentiment.sentiment_extractor import SentimentExctractor
 
 
 def run(
@@ -21,6 +11,7 @@ def run(
     kafka_input_topic: str,
     kafka_output_topic: str,
     kafka_consumer_group: str,
+    sentiment_extractor: SentimentExctractor,
 ):
     """
     Ingests news articles from Kafka and outputs structured outputs with
@@ -30,7 +21,7 @@ def run(
         kafka_input_topic (str): Name of the Kafka topic to read article from.
         kafka_output_topic (str): Name of the Kafka topic to write outputs to.
         kafka_consumer_group (str): Kafka consumer group name.
-
+        sentiment_extractor: The SentimentExtractor object built with BAML to return scores with the chosen LLM model
     Returns:
         None
     """
@@ -51,6 +42,33 @@ def run(
     )
     # Create a streaming dataframe connected to the input topic
     sdf = app.dataframe(topic=news_topic)
+
+    def get_sentiment_scores(news_item: dict) -> List[dict]:
+        """
+        Maps the given `news_item` to the list of sentiment scores.
+
+        Args:
+            news_item (dict): The news item
+
+        Returns:
+            List[dict]: The list of sentiment scores
+        """
+        timestamp_ms = news_item['timestamp_ms']
+        # Description field could not exist according to the API docs
+        news: str = news_item['title']  # + ' ' + news_item.get('description')
+        # Use the LLM sentiment extractor method to map news to a list of SentimentScores
+        output = sentiment_extractor.extract_sentiment_scores(news)
+        # Transform the ouptut from SentimentScores to a list of dictionaries
+        sentiment_scores = [
+            {
+                'coin': score.coin,
+                'score': score.score,
+                'timestamp_ms': timestamp_ms,
+            }
+            for score in output.scores
+        ]
+        return sentiment_scores
+
     sdf = sdf.apply(get_sentiment_scores, expand=True)
     sdf = sdf.update(lambda message: logger.debug(f'Final message: {message}'))
     sdf = sdf.to_topic(topic=news_sentiment_topic)
@@ -62,9 +80,11 @@ def run(
 if __name__ == '__main__':
     from news_sentiment.config import config
 
+    sentiment_extractor = SentimentExctractor(model='ClaudeSonnet4')
     run(
         kafka_broker_address=config.kafka_broker_address,
         kafka_input_topic=config.kafka_input_topic,
         kafka_output_topic=config.kafka_output_topic,
         kafka_consumer_group=config.kafka_consumer_group,
+        sentiment_extractor=sentiment_extractor,
     )
---
+++ services/news-sentiment/src/news_sentiment/sentiment_extractor.py
@@ -0,0 +1,19 @@
+from news_sentiment.baml_client.sync_client import b
+from news_sentiment.baml_client.types import SentimentScores
+
+
+class SentimentExctractor:
+    def __init__(self, model):
+        self.model = model
+
+    def extract_sentiment_scores(self, news: str) -> SentimentScores:
+        return b.ExtractSentimentScores(news)
+
+
+if __name__ == '__main__':
+    sentiment_extractor = SentimentExctractor(model='CustomSonnet4')
+    print(
+        sentiment_extractor.extract_sentiment_scores(
+            'Goldman Sachs says it is going to buy soon 1 M BTC and sell 1 M ETH.'
+        )
+    )
