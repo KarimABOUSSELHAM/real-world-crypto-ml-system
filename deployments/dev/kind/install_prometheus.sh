fe054ef1da8e9685f26ad20c6fe471d4b1875825
Karim Abousselham <karim.abousselham@gmail.com>
2025-08-17 11:50:15 +0100

    Installs prometheus under monitoring namespace in kind cluster

---
+++ deployments/dev/kind/install_prometheus.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm upgrade --install --create-namespace --wait prometheus prometheus-community/prometheus \
+    --namespace=monitoring --values manifests/prometheus-values.yaml
