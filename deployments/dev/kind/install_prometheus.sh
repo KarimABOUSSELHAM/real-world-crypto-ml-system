26d6be4873111800fbe21a87bf260d402bc213e1
Karim Abousselham <karim.abousselham@gmail.com>
2025-08-18 22:03:35 +0100

    Fixes the port mapping to enable promethus communicate with grafana alerting

--- deployments/dev/kind/install_prometheus.sh
+++ deployments/dev/kind/install_prometheus.sh
@@ -3,4 +3,6 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm upgrade --install --create-namespace --wait prometheus prometheus-community/prometheus \
-    --namespace=monitoring --values manifests/prometheus-values.yaml
+    --namespace=monitoring --values manifests/prometheus-values.yaml \
+    --set server.service.port=9090 \
+    --set server.service.targetPort=9090
