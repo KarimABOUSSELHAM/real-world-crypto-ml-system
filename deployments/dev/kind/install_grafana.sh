cefe38bf35c404a54a7b238b4413ee3d550736a6
Karim Abousselham <karim.abousselham@gmail.com>
2025-08-18 22:04:27 +0100

    Sets up grafana alerting into the kind cluster to control the rule of MAE>4 and send the alert throug email

--- deployments/dev/kind/install_grafana.sh
+++ deployments/dev/kind/install_grafana.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-
-helm repo add grafana https://grafana.github.io/helm-charts
-helm upgrade --install --create-namespace --wait grafana grafana/grafana \
+# Check if the `grafana-values.yaml` file contains the `.alertEmails` field
+if yq eval '.alertEmails' manifests/grafana-values.yaml &>/dev/null; then
+    echo "alertEmails field found in grafana-values.yaml"
+    helm repo add grafana https://grafana.github.io/helm-charts
+    echo "Setting Grafana Alerting..."
+    helm upgrade --install --create-namespace --wait grafana grafana/grafana \
     --namespace=monitoring --values manifests/grafana-values.yaml \
-    --set-file dashboards.default.candles-dashboard.json=../../../../dashboards/candles.json
+    --set-file dashboards.default.candles-dashboard.json=../../../dashboards/candles.json
+else
+    helm repo add grafana https://grafana.github.io/helm-charts
+    helm upgrade --install --create-namespace --wait grafana grafana/grafana \
+    --namespace=monitoring --values manifests/grafana-values.yaml \
+    --set-file dashboards.default.candles-dashboard.json=../../../dashboards/candles.json
+fi
